fc1d4febef8758b9a24a271360d500e64352e4f2
igavronski <github@proxima.adm.br>
2022-01-04 09:50:41 -0300

    Add files via upload

---
+++ timer_rele.ino
@@ -0,0 +1,91 @@
+// https://electronics-project-hub.com/digital-clock-using-arduino/#:~:text=Arduino%20Digital%20Clock%20without%20RTC%20The%20circuit%20consists,interested%3A%20Analog%20Clock%20Using%20Servo%20motor%20and%20Arduino
+
+int horaini, horafim;
+int hora, minuto, segundo;
+bool pronto = false, ligado = false;
+const int ajuste = -345;
+const int porta_rele = 10;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(porta_rele, OUTPUT);
+//  Serial.begin(9600);
+  Serial.begin(57600);
+  Serial.setTimeout(10000000);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (!pronto) {
+    desliga_rele();
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  Serial.println("Hora de ligar, hora de desligar:");
+  //while (!Serial.available()) {
+  horaini = Serial.parseInt();
+  horafim = Serial.parseInt();
+  //char buffer0[40];
+  //sprintf(buffer0,"Liga %d e desliga %d",horaini,horafim);
+  //Serial.println(buffer0); 
+  if (Serial.read() == '\n') {
+    Serial.println("Hora:min:seg atuais:");
+    hora = Serial.parseInt();
+    minuto = Serial.parseInt();
+    segundo = Serial.parseInt();
+    //Serial.println("Peguei as horas");
+    char horas[9];
+    sprintf(horas,"%02d:%02d:%02d",hora,minuto,segundo);
+    Serial.println(horas);
+    if (Serial.read() == '\n') { 
+      hora = constrain(hora,0,23);
+      minuto = constrain(minuto,0,59);
+      segundo = constrain(segundo,0,59);
+      pronto = true;
+      digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+    }
+  }
+  //}
+  }
+ if (pronto) relogio();
+}
+
+ void relogio(){
+  char buffer1[80];
+  sprintf(buffer1, "Hora de ligar: %02d Hora de desligar: %02d Hora atual: %02d:%02d:%02d",
+    horaini, horafim, hora, minuto, segundo);
+  Serial.println(buffer1);
+  //Serial.println(segundo);
+  delay(999);
+  delayMicroseconds(1000+ajuste);
+  segundo += 1;
+  if (segundo == 60) {
+    segundo = 0;
+    minuto += 1;
+  }
+  if (minuto == 60) {
+    minuto = 0;
+    hora += 1;
+  }
+  if (hora == 24) {
+    //Serial.println("Virada de dia");
+    hora = 0;
+  }
+  if (hora >= horaini & hora <= horafim & !ligado) {
+    liga_rele();
+  }
+  if (hora >= horafim & ligado) {
+    desliga_rele();
+  }
+}
+
+void liga_rele() {
+  digitalWrite(porta_rele, LOW); // liga led verde e liga porta da esquerda
+  digitalWrite(LED_BUILTIN, HIGH);
+  ligado=true;
+}
+
+void desliga_rele() {
+  digitalWrite(porta_rele, HIGH); // desliga led verde e liga porta da direita
+  digitalWrite(LED_BUILTIN, LOW);
+  ligado=false;
+}
